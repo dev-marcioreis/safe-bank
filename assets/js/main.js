2e54d4108eb0dfa81a7cd87819309a69586166b0
Márcio Reis <122680054+dev-marcioreis@users.noreply.github.com>
2023-05-11 10:01:42 -0300

    country container

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,3 +12,18 @@ const hideMenuRegister = () => {
 
 openMenuRegister.addEventListener('click', showMenuRegister)
 closeMenuRegister.addEventListener('click', hideMenuRegister)
+
+const openMenuCountryBtn = document.getElementById('openMenuCountry')
+const CloseMenuCountryBtn = document.getElementById('closeMenuCountry')
+const menuCountry = document.querySelector('.country__container')
+
+const showCountry = () => {
+    menuCountry.style.bottom = '0'
+}
+
+const hideCountry = () => {
+    menuCountry.style.bottom = '-100%'
+}
+
+openMenuCountryBtn.addEventListener('click', showCountry)
+CloseMenuCountryBtn.addEventListener('click', hideCountry)
